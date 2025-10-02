33806a0dc0886e1c5ead4b9e40810d2e5686cca8
Tens1des <romankotov3213@gmail.com>
2025-10-02 13:00:31 +0300

    Initial Commit

---
+++ TripCheckList/TripCheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripCheckListApp.swift
+//  TripCheckList
+//
+//  Created by Рома Котов on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TripCheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
